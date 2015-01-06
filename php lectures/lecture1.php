ace254c277e7241c8eea24e12201d3cb087dbd7a
Brandon Nguyen <eagleeye2555@gmail.com>
2015-01-06 11:29:52 -0800

    Lecture numero uno

--- php lectures/lecture1.php
+++ php lectures/lecture1.php
@@ -1,1 +1,62 @@
-lecture1.php
+class Bird{
+	//class body
+	//class body
+	//class body
+}
+
+class Airplane{
+	//class body
+	//class body
+	//class body
+}
+
+class BillyBob{
+	//class body
+	//class body
+	//class body
+}
+
+
+$rabbit1 = new Rabbit();
+$rabbit2 = new Rabbit();
+
+$phoenix1 = new Phoenix();
+$phoenix2 = new Phoenix();
+
+$snail1 = new Snail();
+$snail2 = new Snail();
+
+
+class Granny{
+	protected $name = "Abigail";
+	protected $age = "67";
+	protected $occupation = "DayCare Owner";
+	protected $area = "United States";
+}
+
+class Child{
+	private $name = "Gabriel";
+	private $age = "4";
+	private $pastimes = "annoy the living hell out of people";
+	private $favoriteToy = "rubber hammer";
+}
+
+class Mario{
+	public $name = "Mario";
+	public $ethnicity = "Italian";
+	public $occupation = "plumber";
+	public $enemy = "Bowser";
+}
+
+
+$pig1 = new Pig();
+print $pig1­>name;
+// default name
+
+$horse1 = new Horse();
+print $horse->name;
+// default name
+
+$cow1 = new Cow();
+print $cow->name;
+// default name
