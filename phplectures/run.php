836bea13c9640e4c83d4b077512dac8b5a930240
Brandon Nguyen <eagleeye2555@gmail.com>
2015-01-09 08:14:43 -0800

    Finished 3rd Lecture

--- phplectures/run.php
+++ phplectures/run.php
@@ -9,9 +9,13 @@ if (is_bool($dead) === true) {
 	echo "Yup, you found a boolean";
 }
 
+echo "<br>";
+
 if (is_bool($age) === false) {
 	echo "This is not a boolean";
 }
+
+echo "<br>";
 
 // ~~~~~~~~~~~~~~~~~~~Example Dos~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -20,10 +24,25 @@ $sure = array('this', 'is', 'an array');
 echo is_array($sure) ? 'Array' : 'not an Array';
 echo "\n";
 
+echo "<br>";
+
 $notEvenCloseBaby = 'this is a string';
 
 echo is_array($notEvenCloseBaby) ? 'Array' : 'not an Array';
 
+echo "<br>";
+
 // ~~~~~~~~~~~~~~~~~~~Example Tres~~~~~~~~~~~~~~~~~~~~~~~
 
+class Poop{
 
+}
+
+$poop = new Poop;
+
+if(is_object($poop)) {
+	echo "object it is";
+}
+else {
+	echo "object it is not";
+}
