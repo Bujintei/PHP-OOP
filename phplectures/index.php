917d5f3e7d4960d7adbebae6540a91d2bee3ddfa
Brandon Nguyen <eagleeye2555@gmail.com>
2015-01-13 02:24:25 -0800

    Upgraded a few things :)

--- phplectures/index.php
+++ phplectures/index.php
@@ -3,20 +3,25 @@
 class Champion{
 	public $firstName;
 	public $lastName;
-	public $gender;
+	public $awards;
 
-	function __construct($firstName, $lastName, $gender) {
+	function __construct($firstName, $lastName, $awards) {
 		$this->firstName = $firstName;
 		$this->lastName = $lastName;
-		$this->gender = $gender;
+		$this->awards = $awards;
+	}
+
+	function description() {
+		return "The reigning champion 'Bulacke' also known as {$this->firstName}" . " is currently 21 years old. He has won {$this->awards} amount of trophies and currently ranked 1 in North America.";
 	}
 
 	function autographSigning() {
-		return "{$this->firstName}" .
-		"{$this->lastName}";
+		return " {$this->firstName}" . " loves you as a diehard fan. Remember your savior's last name, " .
+		"{$this->lastName}!";
 	}
 
 }
 
-$Champion = new Champion("Blake", "Dayman", "Male");
-print "Champion: {$Champion->autographSigning()}";
+$Champion = new Champion("Blake", "Dayman", "42");
+echo $Champion->description();
+echo $Champion->autographSigning();
