2236c839f8e7217f9e0f9763c4825dc1c7e9145f
Brandon Nguyen <eagleeye2555@gmail.com>
2015-01-15 10:55:42 -0800

    Fixed Errors for only Lecture 2(other lectures don't have any errors)

--- phplectures/lecture2.php
+++ phplectures/lecture2.php
@@ -1,21 +1,20 @@
 <?php
 
-<!-- Example 1 : -->
+/* Example 1 : */
 
 	class Human {
 	public $firstName;
 	public $lastName;
 	public $ethnicity;
 
-		function __construct($title, $firstName, $lastName, $ethnicity) {
+		function __construct($firstName, $lastName, $ethnicity) {
 			$this->firstName = $firstName;
 			$this->lastName = $lastName;
 			$this->ethnicity = $ethnicity;
 		}
 
 		function getName() {
-		return "{$this->firstName}" . 
-		"{$this->lastName}";
+		return $this->firstName . " $this->lastName";
 		}
 	}
 
@@ -24,15 +23,14 @@
 	public $lastName;
 	public $breed;
 
-		function __construct($title, $firstName, $lastName, $breed) {
+		function __construct($firstName, $lastName, $breed) {
 			$this->firstName = $firstName;
 			$this->lastName = $lastName;
 			$this->breed = $breed;
 		}
 
-		function getName() {
-		return "{$this->firstName}" . 
-		"{$this->lastName}";
+		function getNam3() {
+		return $this->firstName . " $this->lastName";
 		}
 	}
 
@@ -41,25 +39,24 @@
 	public $lastName;
 	public $breed;
 
-		function __construct($title, $firstName, $lastName, $breed) {
+		function __construct($firstName, $lastName, $breed) {
 			$this->firstName = $firstName;
 			$this->lastName = $lastName;
 			$this->breed = $breed;
 		}
 
-		function getName() {
-		return "{$this->firstName}" . 
-		"{$this->lastName}";
+		function getN4me() {
+		return $this->firstName . " $this->lastName";
 		}
 	}
 
-<!-- Example 2 : -->
+/* Example 2 : */
 
-$human1 = new Human(“Billy”, “Bob”, “Caucasian”);
-print “Human 1: {$human1­>getName()}\n;
+$human1 = new Human("Billy", "Bob", "Caucasian");
+echo $human1->getName();
 
-$bird1 = new Bird(“Talon”, “Flame”, “Alexandrine Parakeet”);
-print “Bird 1: {$bird1­>getName()}\n;
+$bird1 = new Bird(" Talon", "Flame", "Alexander Parakeet");
+echo $bird1->getNam3();
 
-$cat1 = new Cat(“Kitty”, “Kat”, “Persian Cat”);
-print “Cat 1: {$cat1­>getName()}\n;
+$cat1 = new Cat(" Kitty", "Kat", "Persian Cat");
+echo $cat1->getN4me();
